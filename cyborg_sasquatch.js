d3fefa671df0bf97f5e3d17ff755deba71c2ef42
John Batte <john.batte@icainformatics.com>
2013-11-02 14:35:36 -0500

    replacing incorrect js content

--- cyborg_sasquatch.js
+++ cyborg_sasquatch.js
@@ -1,41 +1,12 @@
-var lib = require('./lib');
-var AUTH = 'YcIimHUXWLPYfViMKRndGneS';
-var USERID = '5273c2e1aaa5cd57a2df6aac';
-var ROOMID = '526a8690aaa5cd07092217f0';
+var express = require("express");
+var app = express();
+app.use(express.logger());
 
-var bot = new lib.Bot(AUTH, USERID, ROOMID);
-
-bot.on('speak', function (data) {
-  var text = data.text;
-  var dj = data.userid;
-  console.log(data.name + ': ' + text);
-  if (text == '/go') {
-    if (lib.tools.checkAccess(dj, bot)) {
-      bot.addDj();
-    }
-  } else if (text == '/stop') {
-    if (lib.tools.checkAccess(dj, bot)) {
-      bot.remDj();
-    }
-  } else if (text == '/skip') {
-    if (lib.tools.checkAccess(dj, bot)) {
-      bot.skip();
-    }
-  } else if (text == '/add') {
-    if (lib.tools.checkAccess(dj, bot)) {
-      bot.playlistAll(function (data) {
-         bot.roomInfo(function(info){
-           var song = info.room.metadata.current_song;
-           console.log("adding " + song.metadata.song + ' by ' + song.metadata.artist);
-           bot.playlistAdd(song._id, data.list.length);
-           console.log("add song: success");
-         });
-       });
-      bot.snag();
-    }
-  }
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
 
-bot.on('newsong', function(data) {
-  bot.bop();
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
